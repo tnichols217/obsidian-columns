8a68aafbd18be357293121955da7c0f8d71968a4
tnichols <tnichols@student.dalat.org>
2022-04-27 13:59:17 +0800

    add list notation

--- README.md
+++ README.md
@@ -2,13 +2,20 @@
 
 Allows you to create columns in Obsidian
 
-Adds two codeblock languages: col and md.
+Adds two codeblock languages: col and col-md.
 The md codeblock is just markdown
 The col codeblock renders each markdown element as its own column.
 - use the md codeblock to group elements as one column
 
+You can also create columns by creating a list in the structure shown:
+- !!!col
+    - (flex-grow)
+    (Text in column 1)
+    - (flex-grow)
+    (Text in column 2)
+
+If the flex-grow value specified is not a number, it defaults to the value specified in styles.css
+
 ## TODO
 
 1. Enable syntax highlighting for editor.
-2. Allow you to modify the flex-grow attribute somehow
-3. Allow you to create columns without codeblocks with special syntax
--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
 import { Plugin, MarkdownRenderChild, MarkdownRenderer } from 'obsidian';
 
+const COLUMNNAME = "col"
+const COLUMNMD = COLUMNNAME + "-md"
+const TOKEN = "!!!"
+
 export default class ObsidianColumns extends Plugin {
 
 	async onload() {
 
-		this.registerMarkdownCodeBlockProcessor("col-md", (source, el, ctx) => {
+		this.registerMarkdownCodeBlockProcessor(COLUMNMD, (source, el, ctx) => {
 			const sourcePath = ctx.sourcePath;
 			let child = el.createDiv();
 			MarkdownRenderer.renderMarkdown(
@@ -15,7 +19,7 @@ export default class ObsidianColumns extends Plugin {
 			);
 		});
 
-		this.registerMarkdownCodeBlockProcessor("col", (source, el, ctx) => {
+		this.registerMarkdownCodeBlockProcessor(COLUMNNAME, (source, el, ctx) => {
 			const sourcePath = ctx.sourcePath;
 			let rows = source.split("\n");
 			let child = createDiv();
@@ -25,13 +29,45 @@ export default class ObsidianColumns extends Plugin {
 				sourcePath,
 				null
 			);
-			console.log(child.children)
 			let parent = el.createEl("div", { cls: "columnParent" });
 			Array.from(child.children).forEach((c) => {
-				let cc = parent.createEl("div", {cls: "columnChild"})
+				let cc = parent.createEl("div", { cls: "columnChild" })
 				cc.appendChild(c)
 			})
-			console.log(parent)
+		});
+
+		this.registerMarkdownPostProcessor((element, context) => {
+			for (let child of Array.from(element.children)) {
+				if (child.nodeName != "UL" && child.nodeName != "OL") {
+					continue
+				}
+				for (let listItem of Array.from(child.children)) {
+					if (!listItem.textContent.startsWith(TOKEN + COLUMNNAME)) {
+						continue
+					}
+					child.removeChild(listItem)
+					let colParent = element.createEl("div", { cls: "columnParent" })
+					let itemList = listItem.querySelector("ul, ol")
+					for (let itemListItem of Array.from(itemList.children)) {
+						let childDiv = colParent.createEl("div", { cls: "columnChild" })
+						let span = parseFloat(itemListItem.textContent.split("\n")[0].split(" ")[0])
+						if (!isNaN(span)) {
+							childDiv.setAttribute("style", "flex-grow:" + span.toString())
+						}
+						console.log(span)
+						console.log([itemListItem.textContent])
+						let afterText = false
+						for (let itemListItemChild of Array.from(itemListItem.childNodes)) {
+							if (afterText) {
+								childDiv.appendChild(itemListItemChild)
+							}
+							if (itemListItemChild.nodeName == "#text") {
+								afterText = true
+							}
+						}
+					}
+				}
+			}
 		});
 	}
 
