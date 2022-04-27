1e01f22526ff7dee114f803dd9be493f627349c4
tnichols <tnichols@student.dalat.org>
2022-04-27 22:18:28 +0800

    add support for nested columns in list notation

--- main.ts
+++ main.ts
@@ -36,27 +36,36 @@ export default class ObsidianColumns extends Plugin {
 			})
 		});
 
-		this.registerMarkdownPostProcessor((element, context) => {
+		let processList = (element: Element) => {
 			for (let child of Array.from(element.children)) {
+				if (child == null) {
+					continue
+				}
 				if (child.nodeName != "UL" && child.nodeName != "OL") {
 					continue
 				}
 				for (let listItem of Array.from(child.children)) {
+					if (listItem == null) {
+						continue
+					}
 					if (!listItem.textContent.startsWith(TOKEN + COLUMNNAME)) {
+						processList(listItem)
 						continue
 					}
 					child.removeChild(listItem)
 					let colParent = element.createEl("div", { cls: "columnParent" })
 					let itemList = listItem.querySelector("ul, ol")
+					if (itemList == null) {
+						continue
+					}
 					for (let itemListItem of Array.from(itemList.children)) {
 						let childDiv = colParent.createEl("div", { cls: "columnChild" })
 						let span = parseFloat(itemListItem.textContent.split("\n")[0].split(" ")[0])
 						if (!isNaN(span)) {
 							childDiv.setAttribute("style", "flex-grow:" + span.toString())
 						}
-						console.log(span)
-						console.log([itemListItem.textContent])
 						let afterText = false
+						processList(itemListItem)
 						for (let itemListItemChild of Array.from(itemListItem.childNodes)) {
 							if (afterText) {
 								childDiv.appendChild(itemListItemChild)
@@ -68,7 +77,9 @@ export default class ObsidianColumns extends Plugin {
 					}
 				}
 			}
-		});
+		}
+
+		this.registerMarkdownPostProcessor((element, context) => {processList(element)});
 	}
 
 	onunload() {
