b10b2e1f35766dad99774815e848ced94dcdaad8
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2022-07-29 22:03:05 +0800

    test
    test
    
    test
    
    test
    
    test
    
    switch from shell to flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Dev shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          nodejs
+          yarn
+          nodePackages.npm
+          nodePackages.nodemon
+        ];
+        buildInputs = [ ];
+      };
+    });
+}
